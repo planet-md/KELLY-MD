674c0eddca1c6490e43385676b672151b04ef029
LOFT-XMD-S23 <sheddysheddy236@gmail.com>
2025-05-08 19:53:53 +0300

    Update Card.js

--- lazackcmds/Card.js
+++ lazackcmds/Card.js
@@ -1,14 +1,14 @@
 let handler = async m =>
   m.reply(
     `⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢸⣿⣿⣷⣜⢿⣧⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡄⠻⣿⣿⣿⣿⣦⠄⠄
-乂───『 *PRINCE BOT MD*』───乂 
-> Library : PRINCE BOT MD
+乂───『 *LOFT XMD S24*』───乂 
+> Library : LOFT XMD
 > Version : 1.0.0
-> source : https://github.com/PRINCETECH20/prince-bot-md
+> source : https://github.com/LOFTXMD23/loft-xmd-s24
 
       if you find error or want to upgrade to premium plan contact to the owner.
       
-> Prince-bot-md is bot made up by Princetech
+> LOFTXMD is bot made up by SIR LOFT 
   __________________________
 
 
