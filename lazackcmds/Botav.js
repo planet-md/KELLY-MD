9455b457077474df210c528481c76ee0574b9af2
LOFT-XMD-S23 <sheddysheddy236@gmail.com>
2025-05-08 19:51:33 +0300

    Update Botav.js

--- lazackcmds/Botav.js
+++ lazackcmds/Botav.js
@@ -13,7 +13,7 @@ conn.sendButton(m.chat, `*HELLO IM HERE*
 conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 } 
 
-handler.customPrefix = /^(princetech|Mickey)$/i
+handler.customPrefix = /^(loftxmd|Mickey)$/i
 handler.command = new RegExp()
 
 export default handler
