db3a3353c524ee780578949cb4b3efc536f1628f
LOFT-XMD-S23 <sheddysheddy236@gmail.com>
2025-05-09 06:10:15 +0300

    Update enable.js

--- lazackcmds/enable.js
+++ lazackcmds/enable.js
@@ -20,54 +20,215 @@ let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isR
         throw false
       }
       chat.welcome = isEnable
-      break
-    case 'jarvis':
-    case 'autotalk':
+      break    
+      
+	  case 'jarvis':
+     case 'autotalk':
       if (m.isGroup) {
         if (!(isAdmin || isOwner)) {
           global.dfail('admin', m, conn)
-          throw false
-        }
-      }
+           throw false
+          }}
       chat.jarvis = isEnable
-      break
-    case 'pmblocker':
-      isAll = true
-      if (!isROwner) {
-        global.dfail('rowner', m, conn)
-        throw false
-      }
-      bot.pmblocker = isEnable
-      case 'antilink2': case 'antienlace2':
-if (m.isGroup) {
-if (!(isAdmin || isOwner)) {
-global.dfail('admin', m, conn)
+     break
+	
+	  
+	  case 'pmblocker':
+	case 'pbm':
+isAll = true
+if (!isROwner) {
+global.dfail('rowner', m, conn)
 throw false
-}}
-chat.antiLink2 = isEnable 
-      break
-    case 'autobio':
-      isAll = true
-      if (!isROwner) {
-        global.dfail('rowner', m, conn)
-        throw false
-      }
-      bot.autoBio = isEnable
-      break
-    case 'detect':
-    case 'detector':
-      if (!m.isGroup) {
-        if (!isOwner) {
-          global.dfail('group', m, conn)
+}
+bot.pmblocker = isEnable
+break	  
+
+		  
+		  case 'testbb':
+	case 'testb':
+isAll = true
+if (!isROwner) {
+global.dfail('rowner', m, conn)
+throw false
+}
+bot.testbot = isEnable
+break	
+
+		  
+
+		  case 'oreact':
+		  case 'ownerreacts':
+	case 'ownerreact':
+isAll = true
+if (!isROwner) {
+global.dfail('rowner', m, conn)
+throw false
+}
+bot.ownerreacts = isEnable
+break	
+
+		  case 'statuslikes':
+		  case 'statuslike':
+		  case 'slikes':
+	case 'likes':
+isAll = true
+if (!isROwner) {
+global.dfail('rowner', m, conn)
+throw false
+}
+bot.like = isEnable
+break	
+
+
+case 'statusreply':
+		  case 'replystatus':
+	case 'sreply':
+isAll = true
+if (!isROwner) {
+global.dfail('rowner', m, conn)
+throw false
+}
+bot.statusreply = isEnable
+break	
+
+
+
+
+		  
+		  case 'sw':
+		  case 'sview':
+	case 'statusview':
+isAll = true
+if (!isROwner) {
+global.dfail('rowner', m, conn)
+throw false
+}
+bot.statusview = isEnable
+break	
+
+
+		  case 'alwaysonline':
+	case 'online':
+isAll = true
+if (!isROwner) {
+global.dfail('rowner', m, conn)
+throw false
+}
+bot.alwaysonline = isEnable
+break	
+
+
+		  case 'statussaver':
+	case 'savestatus':
+		  case 'statussave':
+isAll = true
+if (!isROwner) {
+global.dfail('rowner', m, conn)
+throw false
+}
+bot.statussave = isEnable
+break	
+
+
+case 'anticall':
+        case 'nocall':
+          isAll = true
+if (!isROwner) {
+global.dfail('rowner', m, conn)
+throw false
+}
+bot.anticalls = isEnable
+break
+		  
+
+
+		  
+ case 'autoread': case 'read':
+isAll = true
+if (!isROwner) {
+global.dfail('rowner', m, conn)
+throw false
+}
+bot.autoread2 = isEnable    
+global.opts['autoread'] = isEnable      
+break
+
+
+
+		  
+	case 'antiviewonce':
+	case 'viewonce':
+	  case 'vv':
+isAll = true
+if (!isROwner) {
+global.dfail('rowner', m, conn)
+throw false
+}
+bot.viewonce = isEnable
+break
+
+		  	case 'antiviewonce2':
+	case 'viewonce2':
+	  case 'vv2':
+isAll = true
+if (!isROwner) {
+global.dfail('rowner', m, conn)
+throw false
+}
+chat.viewOnce = isEnable
+break
+
+		  case 'reacts': case 'reaction':
+	case 'autoreaction': case 'reactions': case 'autoreactions':
+isAll = true
+if (!isROwner) {
+global.dfail('rowner', m, conn)
+throw false
+}
+bot.autoreacts = isEnable
+break	  
+
+
+case 'antipmspam':
+	case 'pmspam':
+		  case 'spampm':
+isAll = true
+if (!isROwner) {
+global.dfail('rowner', m, conn)
+throw false
+}
+bot.pmspam = isEnable
+break	  
+		  
+
+	  
+ case 'autobio':
+  isAll = true
+  if (!isROwner) {
+  global.dfail('rowner', m, conn)
+  throw false
+  }
+  bot.autoBio = isEnable
+  break	 
+   
+	  
+	  
+     case 'detect':
+      case 'detector':
+        if (!m.isGroup) {
+         if (!isOwner) {
+           global.dfail('group', m, conn)
           throw false
         }
-      } else if (!isAdmin) {
-        global.dfail('admin', m, conn)
-        throw false
-      }
-      chat.detect = isEnable
-      break
-    case 'autosticker':
+       } else if (!isAdmin) {
+         global.dfail('admin', m, conn)
+         throw false
+       }
+       chat.detect = isEnable
+     break
+    
+	  
+     case 'autosticker':
       if (m.isGroup) {
         if (!(isAdmin || isOwner)) {
           global.dfail('admin', m, conn)
@@ -76,7 +237,10 @@ chat.antiLink2 = isEnable
       }
       chat.autosticker = isEnable
       break
-    case 'antispam':
+      
+	  
+	  
+      case 'antispam':
       if (m.isGroup) {
         if (!(isAdmin || isOwner)) {
           global.dfail('admin', m, conn)
@@ -85,7 +249,9 @@ chat.antiLink2 = isEnable
       }
       chat.antiSpam = isEnable
       break
-    case 'antidelete':
+   
+	  
+   case 'antidelete':
     case 'delete':
       if (m.isGroup) {
         if (!(isAdmin || isOwner)) {
@@ -95,6 +261,8 @@ chat.antiLink2 = isEnable
       }
       chat.delete = !isEnable
       break
+  
+	  
     case 'antitoxic':
     case 'antibadword':
       if (m.isGroup) {
@@ -106,14 +274,17 @@ chat.antiLink2 = isEnable
       chat.antiToxic = isEnable
       break
 
+		  
     case 'document':
     case 'documento':
-      if (m.isGroup) {
+    if (m.isGroup) {
         if (!(isAdmin || isOwner)) return dfail('admin', m, conn)
       }
-      chat.useDocument = isEnable
-      break
-    case 'autostatus':
+    chat.useDocument = isEnable
+    break
+ 
+	  
+	  case 'autostatus':
       isAll = true
       if (!isROwner) {
         global.dfail('rowner', m, conn)
@@ -122,35 +293,229 @@ chat.antiLink2 = isEnable
       chat.viewStory = isEnable
       break
 
-    case 'antilink':
-    case 'antilinkwa':
-    case 'antilinkwha':
+//🤍♥️🤍♥️🤍♥️🤍TEST CHAT		  
+
+
+    case 'testf':
+    case 'testfeature':
+    case 'tst':
       if (m.isGroup) {
         if (!(isAdmin || isOwner)) {
           global.dfail('admin', m, conn)
           throw false
         }
       }
-      chat.antiLink = isEnable
+      chat.testf = isEnable
       break
 
-    case 'nsfw':
-    case '+18':
+
+		  case 'antibot':
+    case 'botanti':
+    case 'nobot':
       if (m.isGroup) {
         if (!(isAdmin || isOwner)) {
           global.dfail('admin', m, conn)
           throw false
         }
       }
-      chat.nsfw = isEnable
+      chat.antiBot = isEnable
       break
 
+		  case 'approve':
+		  case 'ap':
+    case 'autoapprove':
+    case 'approveauto':
+      if (m.isGroup) {
+        if (!(isAdmin || isOwner)) {
+          global.dfail('admin', m, conn)
+          throw false
+        }
+      }
+      chat.autoapprove = isEnable
+      break
+
+
+		  case 'antifake':
+    case 'fake':
+    case 'blockusers':
+      if (m.isGroup) {
+        if (!(isAdmin || isOwner)) {
+          global.dfail('admin', m, conn)
+          throw false
+        }
+      }
+      chat.antifake = isEnable
+      break
+
+
+case 'antideletelinks':
+    case 'deletelinks':
+    case 'linksdelete':
+      if (m.isGroup) {
+        if (!(isAdmin || isOwner)) {
+          global.dfail('admin', m, conn)
+          throw false
+        }
+      }
+      chat.antdeletelinks = isEnable
+      break
+		  
+
+		  case 'nocmds':
+    case 'anticommands':
+    case 'blockcmds':
+      if (m.isGroup) {
+        if (!(isAdmin || isOwner)) {
+          global.dfail('admin', m, conn)
+          throw false
+        }
+      }
+      chat.anticmds = isEnable
+      break
+
+
+		  
+		  case 'antilink2': case 'antilinkall':
+if (m.isGroup) {
+if (!(isAdmin || isOwner)) {
+global.dfail('admin', m, conn)
+throw false
+}}
+chat.antiLink2 = isEnable 
+break
+
+		  
+case 'antitiktok': case 'antitk': case 'antitik':
+if (m.isGroup) {
+if (!(isAdmin || isOwner)) {
+global.dfail('admin', m, conn)
+throw false
+}}
+chat.antiTiktok = isEnable 
+break
+
+		  
+case 'antiyoutube': case 'antiyt':
+if (m.isGroup) {
+if (!(isAdmin || isOwner)) {
+global.dfail('admin', m, conn)
+throw false
+}}
+chat.antiYoutube = isEnable 
+break
+
+		  
+case 'antitelegram': case 'antitl': case 'antitele': case 'antitg': case 'antitel':
+if (m.isGroup) {
+if (!(isAdmin || isOwner)) {
+global.dfail('admin', m, conn)
+throw false
+}}
+chat.antiTelegram = isEnable 
+break
+
+		  
+case 'antifacebook': case 'antifb': case 'antifbook':
+if (m.isGroup) {
+if (!(isAdmin || isOwner)) {
+global.dfail('admin', m, conn)
+throw false
+}}
+chat.antiFacebook = isEnable 
+break
+
+
+		  
+case 'antiinstagram': case 'antinstagram': case 'antiig': case 'antig': case 'antiinsta': case 'antinsta':
+if (m.isGroup) {
+if (!(isAdmin || isOwner)) {
+global.dfail('admin', m, conn)
+throw false
+}}
+chat.antiInstagram = isEnable 
+break
+
+
+		  
+case 'antitwitter': case 'antitw': case 'antitwit': case 'antitwter': case 'antitwiter': case 'antix':
+if (m.isGroup) {
+if (!(isAdmin || isOwner)) {
+global.dfail('admin', m, conn)
+throw false
+}}
+chat.antiTwitter = isEnable 
+break
+
+
+		  
+case 'antidiscord':
+if (m.isGroup) {
+if (!(isAdmin || isOwner)) {
+global.dfail('admin', m, conn)
+throw false
+}}
+chat.antiDiscord = isEnable 
+break
+
+
+		  
+case 'antithreads':
+if (m.isGroup) {
+if (!(isAdmin || isOwner)) {
+global.dfail('admin', m, conn)
+throw false
+}}
+chat.antiThreads = isEnable 
+break
+		  
+
+case 'antitwitch':
+if (m.isGroup) {
+if (!(isAdmin || isOwner)) {
+global.dfail('admin', m, conn)
+throw false
+}}
+chat.antiTwitch = isEnable 
+break
+
+		  case 'antiporn': case 'antinude':
+if (m.isGroup) {
+if (!(isAdmin || isOwner)) {
+global.dfail('admin', m, conn)
+throw false
+}}
+chat.antiPorn = isEnable          
+break
+		  
+      case 'antibotclone':
+      if (m.isGroup) {
+        if (!(isAdmin || isOwner)) {
+          global.dfail('admin', m, conn)
+          throw false
+        }
+      }
+      chat.antiBotClone = isEnable
+      break
+
+		  
+      case 'nsfw':
+      case '+18':
+       if (m.isGroup) {
+         if (!(isAdmin || isOwner)) {
+           global.dfail('admin', m, conn)
+            throw false
+           }}
+    chat.nsfw = isEnable          
+    break
+
+		  
     case 'autolevelup':
-      isUser = true
-      user.autolevelup = isEnable
-      break
+    isUser = true
+     user.autolevelup = isEnable
+     break
 
-    case 'chatbot':
+		  
+     case 'chatbot':
       if (m.isGroup) {
         if (!(isAdmin || isOwner)) {
           global.dfail('admin', m, conn)
@@ -160,6 +525,18 @@ chat.antiLink2 = isEnable
       chat.chatbot = isEnable
       break
 
+    case 'loftchat':
+	case 'loftgpt':	  
+      if (m.isGroup) {
+        if (!(isAdmin || isOwner)) {
+          global.dfail('admin', m, conn)
+          throw false
+        }
+      }
+      chat.Loftchat = isEnable
+      break
+
+		  
     case 'restrict':
     case 'restringir':
       isAll = true
@@ -169,57 +546,53 @@ chat.antiLink2 = isEnable
       }
       bot.restrict = isEnable
       break
-    case 'autotype':
-    case 'alwaysonline':
+
+		  case 'public':
+    case 'publico':
+      isAll = true
+      if (!isROwner) {
+        global.dfail('rowner', m, conn)
+        throw false
+      }
+      global.opts['self'] = !isEnable
+      break
+		  
+	  
+	case 'autotype':
+    case 'type':
       isAll = true
       if (!isOwner) {
         global.dfail('owner', m, conn)
         throw false
       }
       chat.autotype = isEnable
-      break
-
-    case 'anticall':
-    case 'nocall':
+      break         
+      
+    case 'onlypv':
+    case 'onlydm':
+    case 'onlymd':
+    case 'pconly':
       isAll = true
       if (!isOwner) {
         global.dfail('owner', m, conn)
         throw false
       }
-      bot.antiCall = isEnable
+      //global.opts['solopv'] = isEnable
+      bot.pconly = isEnable
       break
-    case 'onlypv':
-    case 'onlydm':
-    case 'onlymd':
-    case 'solopv':
-      isAll = true
-      if (!isROwner) {
-        global.dfail('rowner', m, conn)
-        throw false
-      }
-      global.opts['pconly'] = isEnable
-      break
-
+      
     case 'gponly':
     case 'onlygp':
     case 'grouponly':
-    case 'sologp':
+    case 'gconly':
     case 'sologrupo':
       isAll = true
-      if (!isROwner) {
-        global.dfail('rowner', m, conn)
+      if (!isOwner) {
+        global.dfail('owner', m, conn)
         throw false
       }
-      global.opts['gconly'] = isEnable
-      break
-
-      case 'self':
-      isAll = true
-      if (!isROwner) {
-        global.dfail('rowner', m, conn)
-        throw false
-      }
-      global.opts['self'] = isEnable
+      //global.opts['sologp'] = isEnable
+      bot.gconly = isEnable
       break
 
       
@@ -229,7 +602,7 @@ chat.antiLink2 = isEnable
 ≡ List of options
 
 ━━━━━━━━━━━━━【 *⚙️ BOT SETTINGS* ⚙️ 】━━━━━━━━━━━━━
-                *✨ Toggle Features ✨*
+                *✨ LOFT FEATURE ✨*
 ━━━━━━━━━━━━━━【 *EXAMPLES* 】━━━━━━━━━━━━━━
 
    To toggle features, use *\`${usedPrefix}on\`* or *\`${usedPrefix}off\`*:
