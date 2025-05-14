f8f247bb35afcce4143a7b21bd4f99507891d33a
LOFT-XMD-S23 <sheddysheddy236@gmail.com>
2025-05-14 03:36:22 +0300

    Update Alive.js

--- lazackcmds/Alive.js
+++ lazackcmds/Alive.js
@@ -30,7 +30,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     contextInfo: {
       mentionedJid: [m.sender],
       externalAdReply: {
-        title: 'LOFTXMD',
+        title: 'LOFT XMD POWERFUL WHATSAPP BOT',
         body: 'loft xmd',
         thumbnailUrl: img,
         sourceUrl: 'https://whatsapp.com/channel/0029Vb6B9xFCxoAseuG1g610',
