95f1fecaa26e01e77004e5248370471ba3247c7a
planet-md <princerwanda2004@gmail.com>
2025-05-20 02:31:40 +0300

    Update main-ping.js

--- lazackcmds/main-ping.js
+++ lazackcmds/main-ping.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn }) => {
     key: { fromMe: false, participant: `0@s.whatsapp.net`, remoteJid: 'status@broadcast' },
     message: {
       contactMessage: {
-        displayName: `LOFT_XMD`,
+        displayName: `KELLY MD`,
         vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:'DENZEL MD'\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`,
       },
     },
@@ -26,7 +26,7 @@ let handler = async (m, { conn }) => {
           key: pingMsg.key,
           type: 14,
           editedMessage: {
-            conversation: `*LOFT-Xmd  😈 running ping:* *${latency} ms*`,
+            conversation: `*KELLY MD  ☠️⚡ running ping:* *${latency} ms*`,
           },
         },
       },
